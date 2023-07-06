ccbf0fd41924a3adf6c4e09b7917f3070be051e2
amco1025 <amco1028@naver.com>
2023-07-06 17:46:10 +0900

    07-06

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -8,27 +7,42 @@ function App() {
   let post = '강남 우동 맛집';
   // jsx 문법1 ) h4의 블로그임이 post로 변경
   // document.querySelector('h4').innerHTML = post;
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   let [logo, setLogo] = useState('ReactBlog');
-  let [따봉] = useState(0);
+  let [따봉, 따봉변경] = useState(0);
   // a에 남자 코트 추천 할당
   // b는 state 변경을 도와주는 함수
+
 
   return (
     // return 안에는 병렬로 태그 2개이상 넣지 않기
     <div className="App">
-      <div class="black-nav">
+      <div className="black-nav">
         {/* jsx 문법 3 : 스타일 넣을 땐 style={{스타일명:'값'}} */}
         <h4>{logo}</h4>
       </div>
+
+      <button onClick={()=>{
+        글제목[0] = '여자코트 추천';
+        글제목변경(글제목);
+      }}>글수정</button>
+
       <div className='list'>
-        <h4>{글제목[0]} <span>👍</span> {따봉} </h4>
+        {/* onClick 사용해보기 */}
+        {/* 누를 때 마다 변경되는 값이므로 state로 만들기 */}
+        {/* state를 변경할거면 state 변경함수 꼭 활용하기 */}
+        {/* state 변경함수는 ()안에 입력한걸로 기존 state를 갈아치워준다. */}
+        <h4>{글제목[0]} <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
+
+
       <div className='list'>
         <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+
       <div className='list'>
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
