e2aec25b48bbcd00092ce9d9f969767f8cf562c2
정선재 <amco1028@naver.com>
2023-07-18 17:16:42 +0900

    07-18

---
+++ front-end/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './App.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Routes, Route, Link, useNavigate, Outlet, BrowerRouter as Router } from 'react-router-dom'
+import logo from './assets/image.png';
+
+function App() {
+    let navigate = useNavigate();
+
+    return (
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">
+              <img
+                src={logo} // Use imported image here
+                className="navbar-logo" // Add class here
+                alt="Logo"
+              />
+            </Navbar.Brand>
+            <Nav className="me-auto">
+                <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Routes>
+            <Route path="/" element={<h1>메인페이지</h1>}/>
+            <Route path="/confirm" element={ <confirm/>}/>
+
+        </Routes>
+
+      </div>
+    );
+  }
+  
+  export default App;
