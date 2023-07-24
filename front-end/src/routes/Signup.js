d71a4f54ca277761d0c79be8d2119981378631c8
정선재 <amco1028@naver.com>
2023-07-24 17:30:37 +0900

    07-24

--- front-end/src/routes/Signup.js
+++ front-end/src/routes/Signup.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import './Signup.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function Signup () {
+function Signup() {
   const navigate = useNavigate();
-  
+
   return (
     <div className="container">
-      <div 
-        className="box" 
-        onClick={() => navigate('/signup/student-signup')}
-      >
+      <div className="box" onClick={() => navigate("/signup/student-signup")}>
         학생으로 회원가입
       </div>
-      <div 
-        className="box" 
-        onClick={() => navigate('/signup/teacher-signup')}
-      >
+      <div className="box" onClick={() => navigate("/signup/teacher-signup")}>
         선생님으로 회원가입
       </div>
     </div>
   );
-};
+}
 
 export default Signup;
